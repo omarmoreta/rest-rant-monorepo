88a52e781f5337b26fcf8fffca368f4246a00c3d
Omar <o.moreta@gmail.com>
2024-06-13 02:24:45 -0400

    added session cookies to persist login

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,17 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }) {
-
     const [currentUser, setCurrentUser] = useState(null)
-    // window.setCurrentUser = setCurrentUser
-    // type this into the browser console to see the user logged in as in the navbar
-    // setCurrentUser({
-    //     firstName: "testing",
-    //     lastName: "login"
-    // })
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5000/authentication/profile', {
+                credentials: 'include'
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
