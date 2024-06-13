9f759638557db5e91746f1849856c22bb796120c
Omar <o.moreta@gmail.com>
2024-06-13 01:59:07 -0400

    added sign up and pwd authentication with bcrypt

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -3,9 +3,15 @@ import { createContext, useState } from "react";
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider({ children }){
+function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+    // window.setCurrentUser = setCurrentUser
+    // type this into the browser console to see the user logged in as in the navbar
+    // setCurrentUser({
+    //     firstName: "testing",
+    //     lastName: "login"
+    // })
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
